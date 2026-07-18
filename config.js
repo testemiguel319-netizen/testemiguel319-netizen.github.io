03150b8c52b559fad2cd3a0aab195d97ebd8012d
testemiguel319-netizen <testemiguel319@gmail.com>
2026-07-18 01:57:58 -0300

    Configuração automática do setup

--- config.js
+++ config.js
@@ -1,8 +1,7 @@
 const CONFIG = {
     PROXY_URL: 'https://demolay-proxy.testemiguel319.workers.dev',
-    API_KEY: '',
-    SHEET_ID: '1KC4ESOIz_sm9Kq9wPeDH1fUd4WQpmKbrqVOzxG7LuQY',
-    SCOPES: 'https://www.googleapis.com/auth/spreadsheets profile email',
+    SHEET_ID: '1QALW8b5yfXSVruo34EsdAl_uLSQgbR0gQA5YmSEulPU',
+    SCOPES: 'https://www.googleapis.com/auth/spreadsheets https://www.googleapis.com/auth/drive.file profile email',
     SHEET_LINKS: 'Sheet1',
     SHEET_CONFIG: 'Sheet2',
     RANGE_LINKS: 'Sheet1!A:F',
